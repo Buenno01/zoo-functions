eb94c2ae3ef4ea59be93abff874a8925081f413a
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 11:40:37 -0300

    getSpeciesById function

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const animalsList = [];
+
+  if (ids === undefined) {
+    return animalsList;
+  }
+
+  ids.forEach((id) => {
+    animalsList.push(data.species.find((animal) => animal.id === id));
+  });
+
+  return animalsList;
 };
+
+getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274');
 
 module.exports = getSpeciesByIds;
