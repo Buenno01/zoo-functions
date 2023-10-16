97ace0d67913d2b9d1ef8b7a433c63b8d7c5f804
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 18:55:25 -0300

    [Requisito 11] getEmployeesCoverage retorna a lista de toda a cobertura quando acionada sem parâmetro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+function getGeneralCoverage() {
+  const { employees, species } = data;
+  const generalCoverage = [];
+  employees.forEach((employee) => {
+    const { id, firstName, lastName, responsibleFor } = employee;
+    const locations = [];
+    const animals = [];
+  
+    species.forEach((animal) => {
+      if (responsibleFor.includes(animal.id)){
+        locations.push(animal.location);
+        animals.push(animal.name);
+      }
+    });
+  
+    generalCoverage.push({
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: animals,
+      locations,
+    });
+  });
+
+  return generalCoverage;
+}
+
+const getEmployeesCoverage = (object) => {
+  const generalCoverage = getGeneralCoverage();
+
+  if (!object) {
+    return generalCoverage;
+  }
 };
 
 module.exports = getEmployeesCoverage;
