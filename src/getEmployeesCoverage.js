cbbe41bab5ae538eaadbe1eb6b8fd974a136cd83
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 19:19:06 -0300

    corrected lints

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,26 +1,30 @@
 const data = require('../data/zoo_data');
 
+function returnObject(id, fullName, species, locations) {
+  return ({
+    id,
+    fullName,
+    species,
+    locations,
+  });
+}
+
 function getGeneralCoverage() {
   const { employees, species } = data;
-  const generalCoverage = [];
-  employees.forEach((employee) => {
+  const generalCoverage = employees.map((employee) => {
     const { id, firstName, lastName, responsibleFor } = employee;
     const locations = [];
     const animals = [];
-  
+
     species.forEach((animal) => {
-      if (responsibleFor.includes(animal.id)){
+      if (responsibleFor.includes(animal.id)) {
         locations.push(animal.location);
         animals.push(animal.name);
       }
     });
-  
-    generalCoverage.push({
-      id,
-      fullName: `${firstName} ${lastName}`,
-      species: animals,
-      locations,
-    });
+    const fullName = `${firstName} ${lastName}`;
+
+    return returnObject(id, fullName, animals, locations);
   });
 
   return generalCoverage;
