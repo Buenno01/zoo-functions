868a51f3d0ddb0e9e3792c52f893e145139b8ea2
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 17:47:43 -0300

    [Requisito 9] getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,75 @@
 const data = require('../data/zoo_data');
 
+const getGeneralSchedule = (species) => {
+  const generalSchedule = {};
+  const daysOfTheWeek = [
+    {
+      day: 'Tuesday',
+      officeHour: 'Open from 8am until 6pm'
+    },
+    {
+      day: 'Wednesday',
+      officeHour: 'Open from 8am until 6pm'
+    },
+    {
+      day: 'Thursday',
+      officeHour: 'Open from 10am until 8pm'
+    },
+    {
+      day: 'Friday',
+      officeHour: 'Open from 10am until 8pm'
+    },
+    {
+      day: 'Saturday',
+      officeHour: 'Open from 8am until 10pm'
+    },
+    {
+      day: 'Sunday',
+      officeHour: 'Open from 8am until 8pm'
+    },
+  ];
+
+  daysOfTheWeek.forEach((el) => {
+    generalSchedule[el.day] = {
+      officeHour: el.officeHour,
+      exhibition: [],
+    };
+  });
+
+  generalSchedule.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  }
+  
+  species.forEach((animal) => {
+    animal.availability.forEach((day) => {
+      generalSchedule[day].exhibition.push(animal.name);
+    });
+  });
+
+  return generalSchedule;
+}
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const { species } = data;
+  const specificAnimal = species.find((element) => element.name === scheduleTarget);
+
+  if (specificAnimal) {
+    return specificAnimal.availability;
+  }
+
+  const daysOfTheWeek = [
+    'Sunday', 'Monday', 'Tuesday',
+    'Wednesday', 'Thursday', 'Friday', 'Saturday',
+  ];
+
+  if (daysOfTheWeek.includes(scheduleTarget)) {
+    const temporaryObj = {};
+    temporaryObj[scheduleTarget] = getGeneralSchedule(species)[scheduleTarget];
+    return temporaryObj;
+  }
+
+  return getGeneralSchedule(species);
 };
 
 module.exports = getSchedule;
