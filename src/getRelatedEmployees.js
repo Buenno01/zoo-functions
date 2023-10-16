f0762ff367f5932deeaebc4e4ae9a94d7a3c908b
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 14:33:24 -0300

    [Requisito 4] isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const { employees } = data;
+  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+
+  const actualEmployee = employees.find((person) => person.id === id);
+
+  const managers = actualEmployee.managers;
+
+  if (managers.length == 0 || managers[0] == stephanieId) {
+    return true;
+  }
+
+  return false;
 };
 
 const getRelatedEmployees = (managerId) => {
