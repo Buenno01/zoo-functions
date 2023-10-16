467f6858a22e5dbba26532bdddc03ff3282ce85f
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 16:40:04 -0300

    [Requisito 8] calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const count = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+
+  entrants.forEach((person) => {
+    const ageClassification = person.age < 18
+      ? 'child'
+      : person.age >= 50
+        ? 'senior'
+        : 'adult';
+
+      count[ageClassification] += 1;
+  });
+
+  return count;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  let totalEntry = 0;
+
+  if (entrants && entrants.length > 0) {
+    const count = countEntrants(entrants);
+    const values = {
+      child: 20.99,
+      senior: 24.99,
+      adult: 49.99,
+    };
+
+    ['child', 'senior', 'adult'].forEach((age) => {
+      totalEntry += count[age] * values[age];
+    });
+  }
+
+  return parseFloat(totalEntry.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
