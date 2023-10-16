f8579c514914e43c961d59cf8e77b890c8ac66da
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-16 15:42:16 -0300

    [Requisito 5] função refatorada

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,32 +3,25 @@ const data = require('../data/zoo_data');
 const countAnimals = (animal) => {
   const { species } = data;
 
-  if (!animal) {
-    let returnedObj = {};
-    species.forEach((specie) => {
-      const temporaryObj = {};
-      temporaryObj[specie.name] = specie.residents.length;
+  const eachAnimalCount = {};
+  species.forEach((specie) => {
+    eachAnimalCount[specie.name] = specie.residents.length;
+  });
 
-      returnedObj = { ...returnedObj, ...temporaryObj };
-    });
-    return returnedObj;
+  if (!animal) {
+    return eachAnimalCount;
   }
 
   if (animal.sex) {
     let counter = 0;
-    const object = species.find((e) => e.name === animal.species);
-    object.residents.forEach((individual) => {
-      counter += individual.sex === animal.sex
-        ? 1
-        : 0;
+    const { residents } = species.find((e) => e.name === animal.species);
+    residents.forEach((individual) => {
+      counter += individual.sex === animal.sex ? 1 : 0;
     });
     return counter;
   }
 
-  if (animal.species) {
-    return species.find((e) => e.name === animal.species).residents.length;
-  }
-  // seu código aqui
+  return eachAnimalCount[animal.species];
 };
 
 module.exports = countAnimals;
